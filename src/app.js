d37c272aec3c779b77b78642ed72d0433fe80916
IrvinMitre <mitre_ismm@outlook.com>
2023-11-08 12:11:36 -0600

    Feature: Nodemon

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Hello Liverpool Team!");
 });
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+  console.log("Server running on port 3000");
 });
